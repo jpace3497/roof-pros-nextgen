76d3b32957d7ad6643b3e71a39834b9be9ebec4a
jpace3497 <jpace3497@gmail.com>
2026-03-06 21:54:34 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import DemoPage from "./pages/[company]";
 
 function Home() {
 return (
-<div style={{ padding: "40px", fontFamily: "Arial" }}> <h1>Roof Demo Generator</h1> <p>Demo generator for roofing companies.</p> </div>
+<div style={{ padding: "40px", fontFamily: "Arial" }}> <h1>Roof Demo</h1> <p>Demo generator for roofing companies.</p> </div>
 );
 }
 
 export default function App() {
 return ( <Router> <Routes>
 <Route path="/" element={<Home />} />
-<Route path="/:company" element={<DemoPage />} /> </Routes> </Router>
+<Route path=":company" element={<DemoPage />} /> </Routes> </Router>
 );
 }
